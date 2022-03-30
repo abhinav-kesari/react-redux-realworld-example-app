de1596ea5fa463379c002005ecb82228fe6fb32e
abhinav-kesari <60466018+abhinav-kesari@users.noreply.github.com>
2022-03-30 22:15:18 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://3.110.132.68:3000/api';
+const API_ROOT = 'http://3.110.134.168:3000/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
