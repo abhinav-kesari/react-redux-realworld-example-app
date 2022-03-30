2a3a103455fefa815fb99e1865e05ce3f75b4c67
abhinav-kesari <60466018+abhinav-kesari@users.noreply.github.com>
2022-03-30 22:45:36 +0530

    updated api-root

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://3.110.134.168:3000/api';
+const API_ROOT = 'https://ankur100.squareops.co.in/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
